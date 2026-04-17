10d10cc8c7e8010fbf84c5e1d5c7ffc3ea815cb6
Brayan127814 <jesus.brayancastillo15gmail.com>
2026-04-16 22:42:43 -0500

    feat: coreccion de url base

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  api: "//https://planillajevehicular-1.onrender.com"
+  api: "https://planillajevehicular-1.onrender.com"
   //http://localhost:8082/
   
 };
