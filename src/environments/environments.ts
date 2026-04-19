e1b2c5007826886ca0b51af01606f550032c0c3f
Brayan127814 <jesus.brayancastillo15gmail.com>
2026-04-19 11:06:11 -0500

    feat:

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  api: "//https://planillajevehicular-1.onrender.com"
+  api: "https://planillajevehicular-1.onrender.com"
   //http://localhost:8082
   //https://planillajevehicular-1.onrender.com
   
