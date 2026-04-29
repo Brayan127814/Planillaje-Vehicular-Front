339025a174098e4c5b7bc89dda66c9fd9287b59b
Brayan127814 <jesus.brayancastillo15gmail.com>
2026-04-29 00:44:28 -0500

    fix: cambio a url de produccion

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  api: "http://localhost:8082"
+  api: "https://planillajevehicular-1.onrender.com"
   //http://localhost:8082
   //https://planillajevehicular-1.onrender.com
   
